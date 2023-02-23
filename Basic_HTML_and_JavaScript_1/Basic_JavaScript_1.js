765977d7c3b508596a56bd4c92d17b0166459b81
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-02-23 12:12:35 -0800

    Update Basic_JavaScript_1.js

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -2,5 +2,11 @@
   <html>
   <script>
   alert("Hello, World this is an alert box!");
+  function My_first_function() {
+    var string = "Hello!, Im Irish!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = 
+      result; 
+  }
 </script>
 </html>
